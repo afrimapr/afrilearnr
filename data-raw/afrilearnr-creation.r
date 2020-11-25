024d2a248874c0c9a518a9dbafa56d0dc10c3d7b
AndySouth <southandy@gmail.com>
2020-11-25 15:50:00 +0000

    documentation

--- data-raw/afrilearnr-creation.r
+++ data-raw/afrilearnr-creation.r
@@ -18,15 +18,20 @@ usethis::create_package("afrilearnr") #to make a basic package.
 #usethis::use_tutorial("<name-of-learnr-file>", "<Title You'd Like the User to See>")
 
 setwd('afrilearnr')
-usethis::use_tutorial("intro-to-sf", "An afrimapr intro to sf")
+#usethis::use_tutorial("intro-to-sf", "An afrimapr intro to sf")
 
 #restart rstudio for package build tools to appear
 
-#usethis::use_dev_package("gradethis") #to add package dependencies for in-development packages, like gradethis.
+remotes::install_github('afrimapr/afrilearndata')
+usethis::use_dev_package("afrilearndata") # to add in development package dependencies
+
+# aha mayne then learners will not need to install from github ??
 
 # add cran package dependencies
-usethis::use_package("rnaturalearth")
+#usethis::use_package("rnaturalearth")
 usethis::use_package("sf")
+usethis::use_package("raster")
+
 
 # create data-raw to save this script that creates the package
 usethis::use_data_raw()
