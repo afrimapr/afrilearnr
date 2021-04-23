e873ad5e34898123a6ebbca3f161d58974097802
AndySouth <southandy@gmail.com>
2021-04-23 16:30:59 +0100

    usethis::use_tutorial("test-learnr-pdf", "reprex for failing to print all sections to pdf")

--- data-raw/afrilearnr-creation.r
+++ data-raw/afrilearnr-creation.r
@@ -45,6 +45,9 @@ usethis::use_tutorial("test-sf", "test shinyapps not finding packages in join tu
 # add a test tutorial to try to address issue in join not recognising packages
 usethis::use_tutorial("test-tmap-view", "test tmap failing in view mode when run by learner")
 
+# add a test tutorial reprex for failing to print all sections to pdf
+usethis::use_tutorial("test-learnr-pdf", "reprex for failing to print all sections to pdf")
+
 # add tutorial on getting data in
 usethis::use_tutorial("get-my-data-in", "How to get your own spatial data into R")
 
